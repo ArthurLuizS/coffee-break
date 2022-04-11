c88760c849ee1c65d7fb326e8020825e4d928887
Arthur Luiz <arthurluiz36@gmail.com>
2022-04-11 14:19:12 -0300

    Acrescentado as funcoes de Put e Delete de funcionarios

--- src/main/java/cafeMatinal/controller/FuncionarioController.java
+++ src/main/java/cafeMatinal/controller/FuncionarioController.java
@@ -2,8 +2,13 @@ package cafeMatinal.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +19,13 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin
 public class FuncionarioController {
 
 	private FuncionarioRepository funcionarioRepository ;
 	private FuncionarioService funcionarioService;
 
+	
 	
 	@GetMapping("/funcionarios")
 	public List<Funcionario> listar(){
@@ -26,7 +33,29 @@ public class FuncionarioController {
 	}
 	@PostMapping("/funcionarios/cadastrar")
 	public Funcionario cadastrar(@RequestBody Funcionario funcionario) {
+		
 		Funcionario funcionarioNovo = funcionarioService.cadastrar(funcionario);
+		
 		return funcionarioNovo;
 	}
+	@PutMapping("funcionarios/atualizar/{IdFuncionario}")
+	public ResponseEntity<Funcionario> atualizar(@PathVariable Long IdFuncionario,
+			@RequestBody Funcionario funcionario) {
+		if(!funcionarioRepository.existsById(IdFuncionario)) {
+			return ResponseEntity.notFound().build();
+		}
+		funcionario.setId(IdFuncionario);
+		funcionarioService.cadastrar(funcionario);
+		
+		return ResponseEntity.ok(funcionario);
+	}
+	@DeleteMapping("funcionarios/{IdFuncionario}")
+	public ResponseEntity<Funcionario> excluir(@PathVariable Long IdFuncionario){
+		if(!funcionarioRepository.existsById(IdFuncionario)) {
+			return ResponseEntity.notFound().build();
+		}
+		funcionarioRepository.deleteById(IdFuncionario);
+		return ResponseEntity.noContent().build();
+	}
+
 }
